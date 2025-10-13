1ffdbadee7a35e751db2669863c7067ee5dc2bf4
Sandesh Rai <pumasandeshrai@gmail.com>
2025-10-13 16:12:59 +0545

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AdminDashboard from './components/Admin/AdminDashboard';
 import AdminLoginPage from './components/Admin/AdminLoginPage';
 import Chart from './components/Admin/Chart';
 import Home from './pages/Home';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { UserData } from './Dataprovider/ShareData';
 
 
@@ -11,15 +11,17 @@ const App = () =>{
   console.log('Token data in App:', data);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={data?.message === "Logged In" ? <Navigate to="/admindashboard" /> : <AdminLoginPage />} />
-        <Route path="/admindashboard" element={data?.message === "Logged In" ? <AdminDashboard /> : <Navigate to="/admin" />}>
-          <Route path="chart" element={data?.message === "Logged In" ? <Chart /> : <Navigate to="/admin" />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+
+<HashRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/admin" element={data?.message === "Logged In" ? <Navigate to="/admindashboard" /> : <AdminLoginPage />} />
+    <Route path="/admindashboard" element={data?.message === "Logged In" ? <AdminDashboard /> : <Navigate to="/admin" />}>
+      <Route path="chart" element={data?.message === "Logged In" ? <Chart /> : <Navigate to="/admin" />} />
+    </Route>
+  </Routes>
+</HashRouter>
+
   );
 };
 
